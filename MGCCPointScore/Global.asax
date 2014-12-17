5fb09d0eb4e92c5d902d6643468b704de61741c1
John D Roach <roach.john@gmail.com>
2014-12-17 22:32:49 +1100

    Sample MVC5 app

---
+++ MGCCPointScore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MGCCPointScore.MvcApplication" Language="C#" %>
---
+++ MGCCPointScore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MGCCPointScore.Startup))]
+namespace MGCCPointScore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
