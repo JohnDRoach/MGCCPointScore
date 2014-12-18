195e070d0da9bd28d24978cf74531509584bcd80
John D Roach <roach.john@gmail.com>
2014-12-18 21:55:32 +1100

    MongoDB Test
    HomeController has a call to the MongoDB, here's hoping it works!

--- MGCCPointScore/Controllers/HomeController.cs
+++ MGCCPointScore/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MGCCPointScore.Models;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +11,22 @@ namespace MGCCPointScore.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly MongoDatabase database;
+
+        public HomeController()
+        {
+            var connectionstring = ConfigurationManager.AppSettings.Get("MONGOLAB_URI");
+            var url = new MongoUrl(connectionstring);
+            var client = new MongoClient(url);
+            var server = client.GetServer();
+            database = server.GetDatabase(url.DatabaseName);
+
+            var collection = database.GetCollection<MongoTestObject>("TestTableQuestionMark");
+
+            // insert object
+            collection.Insert(new MongoTestObject { Name = "This came from mongo!" });
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -15,7 +34,11 @@ namespace MGCCPointScore.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            // fetch all objects
+            var collection = database.GetCollection<MongoTestObject>("TestTableQuestionMark");
+            var thingies = collection.FindAll();
+
+            ViewBag.Message = thingies.First().Name;
 
             return View();
         }
