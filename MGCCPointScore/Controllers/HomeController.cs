0d0bc80e6d32ef08a5220b2f9236581ea70276c1
John D Roach <roach.john@gmail.com>
2014-12-29 22:09:25 +1100

    Testing some simple Authentication with Mongo

--- MGCCPointScore/Controllers/HomeController.cs
+++ MGCCPointScore/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using MGCCPointScore.Models;
+﻿using MGCCPointScore.Database;
+using MGCCPointScore.Models;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,20 +13,18 @@ namespace MGCCPointScore.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly MongoDatabase database;
-
         public HomeController()
         {
-            var connectionstring = ConfigurationManager.AppSettings.Get("MONGOLAB_URI");
-            var url = new MongoUrl(connectionstring);
-            var client = new MongoClient(url);
-            var server = client.GetServer();
-            database = server.GetDatabase(url.DatabaseName);
+            //var connectionstring = ConfigurationManager.AppSettings.Get("MONGOLAB_URI");
+            //var url = new MongoUrl(connectionstring);
+            //var client = new MongoClient(url);
+            //var server = client.GetServer();
+            //database = server.GetDatabase(url.DatabaseName);
 
-            var collection = database.GetCollection<MongoTestObject>("TestTableQuestionMark");
+            //var collection = database.GetCollection<MongoTestObject>("TestTableQuestionMark");
 
-            // insert object
-            collection.Insert(new MongoTestObject { Name = "This came from mongo!" });
+            //// insert object
+            //collection.Insert(new MongoTestObject { Name = "This came from mongo!" });
         }
 
         public ActionResult Index()
@@ -35,10 +35,19 @@ namespace MGCCPointScore.Controllers
         public ActionResult About()
         {
             // fetch all objects
-            var collection = database.GetCollection<MongoTestObject>("TestTableQuestionMark");
+            var database = MyMongoDB.Database;
+            var collection = MyMongoDB.UserCollection;
+            
             var thingies = collection.FindAll();
 
-            ViewBag.Message = thingies.First().Name;
+            StringBuilder bob = new StringBuilder();
+
+            foreach(var thing in thingies)
+            {
+                bob.AppendFormat("{0} : {1}\n", thing.UserName, thing.Id);
+            }
+
+            ViewBag.Message = bob.ToString();
 
             return View();
         }
---
+++ MGCCPointScore/Database/MyMongoDB.cs
@@ -0,0 +1,46 @@
+﻿using MGCCPointScore.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace MGCCPointScore.Database
+{
+    public static class MyMongoDB
+    {
+        private const string UserCollectionString = "User";
+        private static MongoDatabase database = null;
+
+        public static MongoCollection<ApplicationUser> UserCollection
+        {
+            get
+            {
+                return database.GetCollection<ApplicationUser>(UserCollectionString);
+            }
+        }
+
+        public static MongoDatabase Database
+        {
+            get 
+            { 
+                if(database == null)
+                {
+                    CreateDBConnection();
+                }
+
+                return database; 
+            }
+        }
+
+        private static void CreateDBConnection()
+        {
+            var connectionstring = ConfigurationManager.AppSettings.Get("MONGOLAB_URI");
+            var url = new MongoUrl(connectionstring);
+            var client = new MongoClient(url);
+            var server = client.GetServer();
+            database = server.GetDatabase(url.DatabaseName);
+        }
+    }
+}
